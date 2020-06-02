2957b635539a637561273511506910a72dee7ed9
Vipul Wairagade <vipulw@winjit.com>
2020-06-02 23:07:21 +0530

    Change Requirements #4

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,9 @@
 <template>
   <nav class="navbar is-fixed-top is-transparent">
     <div class="navbar-brand">
-      <a class="navbar-item" href="#" style="padding-right: 0px" id="topBar">
+      <a class="navbar-item navbar-center" href="#" style="padding-right: 0px" id="topBar">
         <span>Pralay Ramteke</span>
       </a>
-      <div
-          class="navbar-item is-hidden-desktop is-right"
-          style="height: 3.25rem;"
-        >
-          <a
-            class="navbar-item"
-            href="docs/ResumePralayRamteke.pdf"
-            target="_blank"
-            style="padding-left: 0px"
-          >
-            <span class="icon">
-              <i class="fa fa-file-text" title="Resume"></i>
-            </span>
-            <span>CV</span>
-          </a>
-        </div>
       <div
         class="navbar-burger burger"
         data-target="navbarExampleTransparentExample"
@@ -38,56 +22,58 @@
       :class="{ 'is-active': showNav }"
     >
       <div class="navbar-end">
+        <scrollactive active-class="active" :modifyUrl="options.modifyUrl" :offset="options.offset" :duration="options.duration" class="my-nav">
         <a
-          class="navbar-item"
-          href="#"
-          v-scroll-to="'#education'"
+          class="navbar-item scrollactive-item"
+          href="#education"
           @click="showNav = !showNav"
         >
-          <span>🎓Education</span>
+          <span class="icon">🎓</span>
+          <span>Education</span>
         </a>
         <a
-          class="navbar-item"
-          href="#"
-          v-scroll-to="'#experience'"
+          class="navbar-item scrollactive-item"
+          href="#experience"
           @click="showNav = !showNav"
         >
-          <span>👨‍💻Experience</span>
+          <span class="icon">👨‍💻</span>
+          <span>Experience</span>
         </a>
         <a
-          class="navbar-item"
-          href="#"
-          v-scroll-to="'#projects'"
+          class="navbar-item scrollactive-item"
+          href="#projects"
           @click="showNav = !showNav"
         >
-          <span>🛠Projects</span>
+          <span class="icon">🛠</span>
+          <span>Projects</span>
         </a>
         <a
-          class="navbar-item"
-          href="#"
-          v-scroll-to="'#blogs'"
+          class="navbar-item scrollactive-item"
+          href="#blogs"
           @click="showNav = !showNav"
         >
-          <span>✍️Blogs</span>
+          <span class="icon">✍️</span>
+          <span>Blogs</span>
         </a>
         <a
-          class="navbar-item"
-          href="#"
-          v-scroll-to="'#contact'"
+          class="navbar-item scrollactive-item"
+          href="#contact"
           @click="showNav = !showNav"
         >
-          <span>🤝Contact</span>
+          <span class="icon">🤝</span>
+          <span>Contact</span>
         </a>
         <a
           href="docs/ResumePralayRamteke.pdf"
-          class="navbar-item is-hidden-mobile"
+          class="navbar-item"
           target="_blank"
         >
-          <span>
-            <img id="resumeIcon" src="img/resume-ico.png" />
+          <span class="resumeIcon">
+            <img id="resumeIcon" src="img/resume.png" />
           </span>
           <span>CV</span>
         </a>
+        </scrollactive>
       </div>
     </div>
   </nav>
@@ -98,27 +84,52 @@ export default {
   data() {
     return {
       showNav: false,
+      options:{
+        offset: 20,
+        duration: 1500,
+        modifyUrl: false,
+      }
     };
   },
 };
 </script>
 
 <style scoped>
-#topBar{
+.active{
+  font-weight: bold;
+}
+.my-nav{
+  display:inherit
+}
+#topBar {
   letter-spacing: 3px;
   font-weight: normal;
   font-size: 25px;
   padding-left: 60px;
 }
-.navbar-item{
-    color: #000000 !important;
-    font-size: 17px;
+.navbar-item {
+  color: #000000 !important;
+  font-size: 17px;
 }
 .navbar {
-    height: 60px;
-    box-shadow: 0px 1px 10px #b7b6b6;
+  height: 60px;
+  box-shadow: 0px 1px 10px #b7b6b6;
 }
-#resumeIcon{
-  padding-top: 5px;
+.resumeIcon {
+  align-items: center;
+  display: inline-flex;
+  justify-content: center;
+  height: 1.5rem;
+  width: 1.5rem;
+  margin-left: -5px;
+  margin-right: 5px;
+}
+.icon {
+  padding-right: 10px;
+}
+.navbar-item.navbar-center {
+    flex-grow: 1;
+    flex-direction: column;
+    justify-content: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueScrollTo from 'vue-scrollto'
+import VueScrollactive from 'vue-scrollactive';
+
+Vue.use(VueScrollactive);
 
 Vue.config.productionTip = false
-Vue.use(VueScrollTo, {
-  container: "body",
-  duration: 2000,
-  easing: "ease",
-  offset: -20,
-  force: true,
-  cancelable: true,
-  onStart: false,
-  onDone: false,
-  onCancel: false,
-  x: false,
-  y: true
-})
+
 
 new Vue({
   router,
